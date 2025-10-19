071ba0253eecb9aedd0307c9d9e4dd47732808b9
Mostafa-Nezar <mostafanezar19@gmail.com>
2025-10-19 16:29:14 +0300

    back to web

--- app/screens/OrdersScreen.jsx
+++ app/screens/OrdersScreen.jsx
@@ -4,6 +4,7 @@ import tw from "twrnc";
 import { useAppContext } from "../context/AppContext";
 import Header from "../components/Header";
 import { useEffect } from "react";
+import { LinearGradient } from "expo-linear-gradient";
 
 const OrdersScreen = () => {
   const navigation = useNavigation();
@@ -12,22 +13,20 @@ const OrdersScreen = () => {
   return (
     <>
     <Header showBack={true} showSearch={false}/>
-    <View style={tw`flex-1 px-4 mb-4`}>
+    <View style={[tw`flex-1 px-4 mb-4`,{backgroundColor:theme.primary}]}>
     <Text style={[tw`text-xl font-bold p-4`, { color: theme.black }]}>My Orders</Text>
       <FlatList
         data={[...orders].sort((a, b) => new Date(b.date) - new Date(a.date))}
         keyExtractor={(item) => item._id}
         renderItem={({ item }) => (
-          <TouchableOpacity
-            onPress={() => navigation.navigate("OrderDetails", { orderId: item._id })}
-            style={[tw`p-4 mb-3 rounded-lg`, { backgroundColor: theme.semiWhite }]}
-          >
+          <TouchableOpacity onPress={() => navigation.navigate("OrderDetails", { orderId: item._id })} activeOpacity={0.9} style={tw`mb-3 rounded-lg overflow-hidden shadow-md`}>
+          <LinearGradient colors={[theme.red, theme.primary, theme.green]}  start={{ x: 1, y: 0 }} end={{ x: 0, y: 1 }} style={tw`p-4`}>
             <Text style={[tw`text-base font-bold`, { color: theme.black }]}>Order ID: {item._id.slice(-6)}</Text>
-            <Text style={[tw`text-sm`, { color: theme.darkGray }]}>Date: {new Date(item.date).toLocaleDateString()}</Text>
-            <Text style={[tw`text-sm font-semibold mt-1`, { color: theme.primary }]}>Total: ${item.total}</Text>
+            <Text style={[tw`text-sm`, { color: theme.white }]}>Date: {new Date(item.date).toLocaleDateString()}</Text>
+            <Text style={[tw`text-sm font-semibold mt-1`, { color: theme.black }]}>Total: ${item.total}</Text>
+          </LinearGradient>
           </TouchableOpacity>
-        )}
-      />
+        )}/>
     </View>
     </>
   );
