dbbe68bca17c82a8e9496963d2992dbb3b494218
Mostafa-Nezar <mostafanezar19@gmail.com>
2026-01-11 05:13:57 +0200

    fix dot

--- app/screens/home/ProfileScreen/components/ProfileHeader.jsx
+++ app/screens/home/ProfileScreen/components/ProfileHeader.jsx
@@ -25,14 +25,14 @@ const ProfileHeader = ({ user, theme, cart, favorites, isUploading, onPickImage 
     </TouchableOpacity>
     <Text style={[tw`text-xl font-bold mb-1`, { color: theme.black }]}>{user?.name}</Text>
     <Text style={[tw`text-base`, { color: theme.darkGray }]}>{user?.email}</Text>
-    <View style={tw`flex-row mt-4`}>
-      <View style={tw`items-center mx-4`}>
-        <Text style={[tw`text-lg font-bold`, { color: theme.primary }]}>{cart.length}</Text>
-        <Text style={[tw`text-sm`, { color: theme.darkGray }]}>In Cart</Text>
+    <View style={[tw`w-3/4 flex-row mt-4 justify-evenly`]}>
+      <View style={tw`flex-row items-center`}>
+        <Icon name="shopping-cart" size={32} color={theme.primary} />
+        <Text style={[tw`text-lg font-bold ml-2`, { color: theme.primary }]}>{cart.length}</Text>
       </View>
-      <View style={tw`items-center mx-4`}>
-        <Text style={[tw`text-lg font-bold`, { color: theme.primary }]}>{favorites.length}</Text>
-        <Text style={[tw`text-sm`, { color: theme.darkGray }]}>Favorites</Text>
+      <View style={tw`flex-row items-center`}>
+        <Icon name="favorite" size={32} color={theme.primary} />
+        <Text style={[tw`text-lg font-bold ml-2`, { color: theme.primary }]}>{favorites.length}</Text>
       </View>
     </View>
   </View>
